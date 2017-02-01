e1fbb5935154d1828d555b996df7a5896e340dfe
phits <john@fitz.com>
2017-02-01 11:02:47 -0500

    Finished anim step 1

--- tutorial_starter/js/main.js
+++ tutorial_starter/js/main.js
@@ -4,8 +4,43 @@
 	'use strict';
 
 	// declare actors here
+    var $backFallingLeaves = $('#brownLeaf, #redLeaf, #orangeLeaf'),
+        $textLine1 = $('.text-line-1'),
+        $textLine2 = $('.text-line-2'),
+        $textGreeting = $('.text-greeting'),
+        $treeLeaves = $('[id^=tree_leaves'),
+        $floorLeaves = $('[id^=floorleaf]'),
+        $bird = $('#Bird'),
+        $birdHat = $bird.find('#BirdHat'),
+        $birdEyes = $bird.find('#leftEye, #rightEye'),
+        $nest = $('#NestAndLeaves'),
+        $tree = $('#tree_trunk'),
+        $cardContainer = $('.card.container')
+        ;
 
 	// clear stage 
+	function clearStage() {
+		var clearTL = new TimelineMax();
+
+		clearTL
+		   .set($backFallingLeaves, {autoAlpha: 0})
+		   .set($textLine1, {autoAlpha: 0})
+		   .set($textLine2, {autoAlpha: 0})
+		   .set($textGreeting, {autoAlpha: 0})
+		   .set($treeLeaves, {autoAlpha: 0})
+		   .set($treeLeaves, {autoAlpha: 0})
+		   .set($bird, {y: '+=65', autoAlpha: 0})
+		   .set($nest, {autoAlpha: 0})
+		   .set($tree, {autoAlpha: 0})
+		   .set($floorLeaves, {y: '+=275', onComplete: showContainer})
+		   ;
+
+		   function showContainer() {
+		   	    $cardContainer.css('display', 'block');
+		   }
+
+		return clearTL
+	}
 
 	// enter floor vegetation
 
@@ -14,6 +49,20 @@
 	// enter the greeting text
 	
 	// the GO function ...to kick things all off
+	function go() {
+		console.log('go ...');
+
+		var masterTL = new TimelineMax();
+    
+        masterTL
+            .add(clearStage(), 'scene-clear-stage')
+
+            ;
+
+		//TODO add child timelines to masterTL
+	}
+
+	go();
 	
 
 })(jQuery);
